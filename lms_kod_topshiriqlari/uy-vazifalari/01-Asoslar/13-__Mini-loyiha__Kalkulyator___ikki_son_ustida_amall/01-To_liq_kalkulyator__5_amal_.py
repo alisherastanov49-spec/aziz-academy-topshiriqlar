91f31cf1ba7e297864c1dee61c850203fe4f82fc
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-07-10 13:55:16 +0500

    Add: To'liq kalkulyator (5 amal) [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-To_liq_kalkulyator__5_amal_.py
@@ -0,0 +1,12 @@
+a = int(input().strip())
+b = int(input().strip())
+yigindi = a + b
+ayirma = a - b
+kopaytma = a * b
+bolinma = a / b
+daraja = a ** b
+print(yigindi)
+print(ayirma)
+print(kopaytma)
+print(bolinma)
+print(daraja)
