abe554e87fe4f705bbe387de77c18c7996aa6709
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-07-10 14:02:53 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,9 @@
+soni = int(input().strip())
+narxi = int(input().strip())
+chegirma = int(input().strip())
+jami = soni * narxi
+chegirma_miqdori = jami * chegirma // 100
+yakuniy = jami - chegirma_miqdori
+print(jami)
+print(chegirma_miqdori)
+print(yakuniy)
