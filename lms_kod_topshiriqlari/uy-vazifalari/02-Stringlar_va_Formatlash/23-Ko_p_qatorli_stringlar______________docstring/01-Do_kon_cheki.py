d8fd3dbebf94dac610cf79ea8a694d1e67d969a5
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-08-12 11:16:50 +0500

    Add: Do'kon cheki [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/23-Ko_p_qatorli_stringlar______________docstring/01-Do_kon_cheki.py
@@ -0,0 +1,5 @@
+mahsulot = input()
+narxi = int(input())
+soni = int(input())
+jami = narxi * soni
+print(f"===== CHEK =====\nMahsulot: {mahsulot}\nNarx: {narxi}\nSoni: {soni}\nJami: {jami}\n================")
