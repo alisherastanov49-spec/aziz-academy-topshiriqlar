c5a415366889aa693a5fb714332a58902cb80d13
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-08-17 21:52:04 +0500

    Add: Barcha to'rt amal [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/01-Barcha_to_rt_amal.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+print(f"Yig'indi: {a + b}")
+print(f"Ayirma: {a - b}")
+print(f"Ko'paytma: {a * b}")
+if b != 0:
+    print(f"Bo'linma: {a // b}")
+else:
+    print("Bo'linma: aniqlanmagan")
