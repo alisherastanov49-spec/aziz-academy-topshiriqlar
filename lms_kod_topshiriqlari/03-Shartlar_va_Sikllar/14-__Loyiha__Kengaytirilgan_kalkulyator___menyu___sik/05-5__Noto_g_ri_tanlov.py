60638e108c03070410c7a20fd5d705d0e21bdb5a
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-08-17 13:14:42 +0500

    Add: 5) Noto‘g‘ri tanlov [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/14-__Loyiha__Kengaytirilgan_kalkulyator___menyu___sik/05-5__Noto_g_ri_tanlov.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n % 2 != 0:
+    if n in [3]:    
+        print("Valid")
+    else:
+        print("Invalid")
+else:
+    if n == 2:
+        print("Valid")
+    else:
+        print("Invalid")
