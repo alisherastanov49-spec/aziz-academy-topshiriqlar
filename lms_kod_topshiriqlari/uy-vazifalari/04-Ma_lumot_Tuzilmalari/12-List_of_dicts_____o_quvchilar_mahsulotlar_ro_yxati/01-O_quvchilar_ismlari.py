dc04b6505a7454e347b4aab896fdf43387f9ff90
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-09-16 14:20:50 +0500

    Add: O'quvchilar ismlari [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/04-Ma_lumot_Tuzilmalari/12-List_of_dicts_____o_quvchilar_mahsulotlar_ro_yxati/01-O_quvchilar_ismlari.py
@@ -0,0 +1,7 @@
+n = int(input())
+oquvchilar = []
+for _ in range(n):
+    ism , yosh = input().split()
+    oquvchilar.append({"ism": ism, "yosh": int(yosh)})
+for oquvchilar in oquvchilar:
+    print(oquvchilar["ism"])
