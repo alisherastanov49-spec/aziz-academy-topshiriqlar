3191e2333e707aefc79decc8b27325ab863bd831
alisherastanov49-spec <alisherastanov49@gmail.com>
2026-09-23 14:21:15 +0500

    Add: 7) Global ro‘yxatga qo‘shish: log [alisherastanov49-spec]

---
+++ lms_kod_topshiriqlari/06-Funksiyalar/05-Scope_____local_vs_global__global_kalit_so_zi/07-7__Global_ro_yxatga_qo_shish__log.py
@@ -0,0 +1,13 @@
+logs = []
+
+def add_log(msg):
+    logs.append(msg)
+    
+n = int(input())
+for _ in range(n):
+    msg = input()
+    add_log(msg)
+    
+print(len(logs))
+for log in logs:
+    print(log)
